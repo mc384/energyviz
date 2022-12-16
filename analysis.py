6471241121879f8a0261be459bf041968debb1e8
Mark Chen <m384chen@uwaterloo.ca>
2022-12-16 18:48:27 -0500

    init

---
+++ analysis.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("energy.csv")
+# print(data.columns)
+# print(data.values)
+US = data.loc[data['Entity'] == "United States"]
+USA = US.rename(
+    columns={"Primary energy consumption per capita (kWh/person)": "kWh"})
+print(USA.head())
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, render_template
+import pandas as pd
+import numpy as np
+import json
+import plotly
+import plotly.express as px
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def energy():
+    data = pd.read_csv("energy.csv")
+    US = data.loc[data['Entity'] == "United States"]
+    USA = US.rename(
+        columns={"Primary energy consumption per capita (kWh/person)": "kWh"})
+    chart = px.bar(USA, x="Year", y="kWh")
+    graphJSON = json.dumps(chart, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("energy.html", graphJSON=graphJSON)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
